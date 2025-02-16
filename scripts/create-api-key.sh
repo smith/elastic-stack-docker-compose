57ee5ca2f4cb5ba857da3cf8c47845b017095f43
Nathan L Smith <nathan.smith@elastic.co>
2025-02-15 20:31:08 -0600

    huh

--- scripts/create-api-key.sh
+++ scripts/create-api-key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
-# TODO: This can fail and give you a yaml file where the key is empty
-test -f /etc/elastic/api_key.yaml || \
+# TODO: This can fail and give you a YAML file where the key is empty
+test -f /etc/elastic/api_key.yml || \
 curl --no-progress-meter --fail -X POST \
     --cacert /usr/share/elasticsearch/config/certs/ca/ca.crt \
     --user "elastic:$ELASTIC_PASSWORD" \
@@ -9,5 +9,5 @@ curl --no-progress-meter --fail -X POST \
     "https://elasticsearch:$ES_PORT/_security/api_key" \
     --data @/etc/elastic/api_key_create_params.json | \
 jq --raw-output .encoded | \
-(echo -ne "exporters:\n  elasticsearch:\n    api_key: " && cat) \
-> /etc/elastic/api_key.yaml
+(echo -ne "exporters:\n  elasticsearch/otel:\n    api_key: " && cat) \
+> /etc/elastic/api_key.yml
